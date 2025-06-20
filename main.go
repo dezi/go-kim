ca54d4634a4cf66ffb4805dc4cf410c363cd1f50
dezi <dezi@kappa-mm.de>
2025-06-20 11:25:22 +0200

    Check in.

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"github.com/dezi/go-kim/utils/log"
 )
 
+// https://code-with-me.global.jetbrains.com/M0DzoQjZ4OzwC2mcjnYeaQ#p=GO&fp=255F29718F419FF233780E6FED9F5C8C3A93EEDEDE03CDD411D247B543F69A7F&newUi=true
+
 func main() {
 
 	log.Printf("Hello world started...")
